9127b49753d506648aeb8f3d3d2cee990e84bb60
ian-ellis <ian.ellis@qantasloyalty.com>
2019-03-30 20:00:43 +1100

    Initial Commit

---
+++ app/src/main/java/com/github/ianellis/shifts/ShiftsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.ianellis.shifts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShiftsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shifts)
+    }
+}
