b0367692d398ed85c82287c62f998b68823ffeef
ian-ellis <ian.ellis@qantasloyalty.com>
2019-03-31 15:26:12 +1100

    Adding ShiftsService
    Adding ShiftService. Basic service interface annotated with retrofit annotations as this will be the primary use case, although these will not stop us providing different implementations when required. Chose to use coroutine Deferred objects as the return type as I intent to use the Coroutine adapter with retrofit to handle the async nature. I personally find the standard CallBack interface quite verbose so would prefer to use the new coroutines. This also enables us to wrap our business rules using coroutines more easily.

---
+++ domain/src/main/java/com/github/ianellis/shifts/domain/ShiftsService.kt
@@ -0,0 +1,21 @@
+package com.github.ianellis.shifts.domain
+
+import com.github.ianellis.shifts.entities.EndShiftRequest
+import com.github.ianellis.shifts.entities.ShiftEntity
+import com.github.ianellis.shifts.entities.StartShiftRequest
+import kotlinx.coroutines.Deferred
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ShiftsService {
+
+    @POST("/dmc/shift/start")
+    fun startShiftAsync(@Body request: StartShiftRequest): Deferred<Unit>
+
+    @POST("/dmc/shift/end")
+    fun endShiftAsync(@Body request: EndShiftRequest): Deferred<Unit>
+
+    @GET("/dmc/shifts")
+    fun shiftAsync(): Deferred<List<ShiftEntity>>
+}
