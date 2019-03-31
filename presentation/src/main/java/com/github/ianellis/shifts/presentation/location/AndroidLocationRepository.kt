cecac944c583b86ed8772bbaf6cf7314cc28ef7b
ian-ellis <ian.ellis@qantasloyalty.com>
2019-04-01 08:55:42 +1100

    Adding AndroidLocationRepository
    A LocationRepository implementation that uses the play services to get the users location. Although we execute the request as a Deferred coroutine on the default dispatcher we need to switch back to the Main thread in order to register our listener. We can the use the suspendCoroutine method to suspend the coroutine until we either receive a result, or error, either resume or resumeWithException Explicitly
    
    Testing OS components like Locations can be quite difficult. I have excluded any unit tests at this point in order to focus on more pressing parts of the code. Strategies to test this include a full blown connectedTest or more ideally a simple Unit test. Whether we can unit test this will depend largely on whether we can mock the location provider. As far as checking permissions we could potentially inject a function that specifically handles this very Android specific part of the logic, or try to use some static mocking on ContextCompat

---
+++ presentation/src/main/java/com/github/ianellis/shifts/presentation/location/AndroidLocationRepository.kt
@@ -0,0 +1,60 @@
+package com.github.ianellis.shifts.presentation.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import com.github.ianellis.shifts.domain.location.LatLng
+import com.github.ianellis.shifts.domain.location.LocationRespoitory
+import com.github.ianellis.shifts.domain.location.LocationUnavailableException
+import com.github.ianellis.shifts.domain.location.PermissionRequiredException
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class AndroidLocationRepository(
+    private val context: Context,
+    private val locationProvider: FusedLocationProviderClient
+) : LocationRespoitory {
+
+    companion object {
+        private const val LOCATION_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION
+    }
+
+    override fun getLocationAsync(): Deferred<LatLng> {
+
+        return GlobalScope.async {
+            if (permissionGranted()) {
+                withContext(Dispatchers.Main) {
+                    getLocationFromManagerAsync()
+                }
+            } else {
+                throw PermissionRequiredException(LOCATION_PERMISSION)
+            }
+        }
+    }
+
+    private fun permissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(context, LOCATION_PERMISSION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private suspend fun getLocationFromManagerAsync(): LatLng {
+        return suspendCoroutine { cont ->
+            locationProvider.lastLocation.addOnCompleteListener { taskResult ->
+                if (taskResult.isSuccessful) {
+                    val location = taskResult.result
+                    cont.resume(LatLng(location.latitude, location.longitude))
+                } else {
+                    cont.resumeWithException(LocationUnavailableException())
+                }
+
+            }
+        }
+    }
+}
