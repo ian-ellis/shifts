a2388e30074d5582ecfd393bdcc18158e9c21954
ian-ellis <ian.ellis@qantasloyalty.com>
2019-04-03 09:09:46 +1100

    Adding ViewModelFactory
    In order to use Dependency Injection with Android ViewModels, you need to provide a custom factory. It has a method that takes a class, and is constructed with a map of class to providers. This allows Android to create the ViewModels using your own providers, but allows the system to manage their lifecycle. We provide the ViewModelKey to be the key to the map of classes to providers

---
+++ presentation/src/main/java/com/github/ianellis/shifts/presentation/di/ViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.github.ianellis.shifts.presentation.di
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import dagger.MapKey
+import javax.inject.Inject
+import javax.inject.Provider
+import kotlin.reflect.KClass
+
+class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = viewModels[modelClass]?.get() as T
+}
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MapKey
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
---
+++ presentation/src/main/java/com/github/ianellis/shifts/presentation/di/ViewModelFactoryModule.kt
@@ -0,0 +1,12 @@
+package com.github.ianellis.shifts.presentation.di
+
+import android.arch.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class ViewModelFactoryModule {
+
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
