2ac9d81af5aab7446eb9ea450700775c57a5982a
ian-ellis <ian.ellis@qantasloyalty.com>
2019-04-03 11:55:57 +1100

    Adding Permissions Handling To ShiftsActivity

--- presentation/src/main/java/com/github/ianellis/shifts/presentation/ShiftsActivity.kt
+++ presentation/src/main/java/com/github/ianellis/shifts/presentation/ShiftsActivity.kt
@@ -1,9 +1,14 @@
 package com.github.ianellis.shifts.presentation
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import com.github.ianellis.shifts.presentation.databinding.ActivityShiftsBinding
 import dagger.android.AndroidInjection
@@ -20,10 +25,49 @@ class ShiftsActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         vm = ViewModelProviders.of(this, viewModelFactory)[ShiftsViewModel::class.java]
+        vm.requestPermission.observe(this, Observer {
+            it?.let(this::handlePermissions)
+        })
         val binding = DataBindingUtil.setContentView<ActivityShiftsBinding>(this, R.layout.activity_shifts)
         binding.lifecycleOwner = this
         binding.viewModel = vm
 
     }
-    
+
+
+    private fun handlePermissions(permission: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(permission)) {
+                showPermissionsDialog(permission)
+            } else {
+                requestPermission(permission)
+            }
+        }
+    }
+
+    private fun requestPermission(permission: String) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), 1)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+            vm.onPermissionGranted?.invoke()
+        }
+    }
+
+    private fun showPermissionsDialog(permission: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.shift_location_permission_dialog_title)
+            .setMessage(R.string.shift_location_permission_dialog_message)
+            .setPositiveButton(R.string.shift_location_permission_dialog_positive) { dialog, which ->
+                requestPermission(permission)
+                dialog.dismiss()
+            }
+            .setNegativeButton(R.string.shift_location_permission_dialog_negative) { dialog, which ->
+                dialog.dismiss()
+            }.show()
+    }
+
+
 }
