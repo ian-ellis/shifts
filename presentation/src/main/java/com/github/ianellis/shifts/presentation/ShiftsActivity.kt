75b4e010196c1ff59210a1de1a8405b30fdb13af
ian-ellis <ian.ellis@qantasloyalty.com>
2019-04-03 11:37:13 +1100

    Updating ShiftsActivity to use data binding with injected view model
    Rather than inject the VM directly we need to inject the factory and fetch it using the class as the key. This allows android to manage the lifecycle giving us back the same instance even if it is a different activity (through device rotation for example). We also need to make the call to AndroidInjection.inject, important to note that this is called before onCreate.
    
    Another common gotcha is forgetting to set the lifecycle owner of the bindings. Forgetting to do this results in updates to the bounds data not being presented

--- presentation/src/main/java/com/github/ianellis/shifts/presentation/ShiftsActivity.kt
+++ presentation/src/main/java/com/github/ianellis/shifts/presentation/ShiftsActivity.kt
@@ -1,14 +1,29 @@
 package com.github.ianellis.shifts.presentation
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.ianellis.shifts.presentation.databinding.ActivityShiftsBinding
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 class ShiftsActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var vm: ShiftsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shifts)
+        vm = ViewModelProviders.of(this, viewModelFactory)[ShiftsViewModel::class.java]
+        val binding = DataBindingUtil.setContentView<ActivityShiftsBinding>(this, R.layout.activity_shifts)
+        binding.lifecycleOwner = this
+        binding.viewModel = vm
 
     }
-
+    
 }
